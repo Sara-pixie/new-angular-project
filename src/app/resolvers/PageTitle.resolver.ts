340096cccdfff26c6beb76a2261611d5077b9c2d
Sara Paranuk <78561671+Sara-pixie@users.noreply.github.com>
2023-11-07 11:20:52 +0100

    page title resolver cleanup

--- src/app/resolvers/PageTitle.resolver.ts
+++ src/app/resolvers/PageTitle.resolver.ts
@@ -5,8 +5,6 @@ const PageTitlesByURL = new Map()
 
 export const PageTitleResolver: ResolveFn<string> =
 (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
-  const url = state.url;
   const pageTitle = PageTitlesByURL.get(state.url);
-  console.log('url',url,'pageTitle',pageTitle);
   return pageTitle ? pageTitle : '404 - page title not set';
 };
