bc22be84a12f205a38ee30ce4e0c27c3afbf9438
Sara Paranuk <78561671+Sara-pixie@users.noreply.github.com>
2023-11-08 12:02:36 +0100

    Editing mat headers

--- src/app/components/title/title.component.ts
+++ src/app/components/title/title.component.ts
@@ -1,16 +1,18 @@
 import { TranslateModule } from '@ngx-translate/core';
 import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
     selector: 'app-title',
     templateUrl: './title.component.html',
     styleUrls: ['./title.component.scss'],
     standalone: true,
-    imports: [TranslateModule]
+    imports: [TranslateModule, NgClass]
 })
 export class TitleComponent implements AfterViewInit {
   @ViewChild('titleElementRef') titleElementRef!: ElementRef;
   @Input({ required: true }) title: string = '';
+  @Input() removeUnderline: boolean = false;
 
   constructor() {}
 
